7feb231c364d333bbfa4329ba4ad87aac156937b
adrianacp10 <144694058+adrianacp10@users.noreply.github.com>
2023-12-17 17:02:59 +0000

    final

--- src/app.js
+++ src/app.js
@@ -6,16 +6,13 @@ window.onload = function() {
   let noun = ["jogger", "racoon", "uncle", "father", "panda", "cat"];
   let domain = [".com", ".es", ".org", ".cat"];
 
-  let domainName = [];
-
   for (let pronoun of pronoun) {
     for (let adj of adj) {
       for (let noun of noun) {
         for (let domain of domain) {
-          domainName.push(`${pronoun}${adj}${noun}${domain}`);
+          console.log(`${pronoun}${adj}${noun}${domain}`);
         }
       }
     }
   }
-  console.log(domainName);
 };
